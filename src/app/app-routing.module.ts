e4a3a4ce288e15aa84627e7da93080cca5e72034
George Petrakis <georgepetrakis99@gmail.com>
2021-05-31 14:41:22 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './@core/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: LandingComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
