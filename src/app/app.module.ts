efbcadb1b351b57ac14676f938499097d2e7b3c8
George Petrakis <georgepetrakis99@gmail.com>
2021-05-31 14:19:24 +0300

    imports - flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './@nav/header/header.component';
 import { FooterComponent } from './@nav/footer/footer.component';
 
+
+//Material design
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+
+//flex layout
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +30,18 @@ import { FooterComponent } from './@nav/footer/footer.component';
     FooterComponent
   ],
   imports: [
+    FlexLayoutModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatInputModule,
+    MatGridListModule,
+    MatSelectModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
